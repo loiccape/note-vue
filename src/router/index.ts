f9c8b7fd60e49fb023ace3610a7dd3a5c548a25d
Loïc Cape <loiccape.pro@outlook.fr>
2024-11-17 12:25:06 +0100

    add forgotten route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,15 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
-import Login from '@/views/Login.vue'
+import Login from '@/views/connexions/Login.vue'
+import Forgotten from '@/views/connexions/Forgotten.vue'
 
 const routes = [
-  { path: '/', name:"login" , component: Login },
+   // Default route: Redirect to '/connexion/login'
+   { path: '/', redirect: '/connexion/login' },
+
+  // Connexions routes
+  { path: '/connexion/login', name:"login" , component: Login },
+  { path: '/connexion/forgotten', name:"forgotten" , component: Forgotten },
 ]
 
 const router = createRouter({
---
+++ src/views/connexions/Forgotten.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Notes</h1>
+        <span>Please log in to continue</span>
+
+        <form @submit.prevent="handleSubmit">
+            <label for="email">Email Address</label>
+            <input type="email" id="email" v-model="data.email" />
+            <label for="password">Password</label>
+            <div>
+                <input type="password" id="password" v-model="data.password" />
+                <a href="#">Forgot</a>
+            </div>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+<script setup>
+</script>
