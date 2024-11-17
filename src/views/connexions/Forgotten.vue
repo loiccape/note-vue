92829fc3110dcd5b69583fc85e10a4e225dc81f9
Loïc Cape <loiccape.pro@outlook.fr>
2024-11-17 12:33:46 +0100

    done

--- src/views/connexions/Forgotten.vue
+++ src/views/connexions/Forgotten.vue
@@ -1,19 +1,25 @@
 <template>
     <div>
-        <h1>Notes</h1>
-        <span>Please log in to continue</span>
+        <h2>Forgotten your password</h2>
+        <p>Enter your email below, and we’ll send you a link to reset it.</p>
 
         <form @submit.prevent="handleSubmit">
             <label for="email">Email Address</label>
-            <input type="email" id="email" v-model="data.email" />
-            <label for="password">Password</label>
-            <div>
-                <input type="password" id="password" v-model="data.password" />
-                <a href="#">Forgot</a>
-            </div>
+            <input type="email" id="email" v-model="data.email" placeholder="email@example.com"/>
             <button type="submit">Login</button>
         </form>
     </div>
 </template>
 <script setup>
+
+import { reactive } from 'vue';
+
+let data = reactive({
+    email:""
+})
+
+let handleSubmit = () => {
+    console.log(data);
+    
+}
 </script>
