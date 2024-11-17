bf61d984f25cfd5683c6d681debe7c78ab6b1fc4
Loïc Cape <loiccape.pro@outlook.fr>
2024-11-17 12:33:17 +0100

    update router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 
 import Login from '@/views/connexions/Login.vue'
 import Forgotten from '@/views/connexions/Forgotten.vue'
+import Home from '@/views/Home.vue'
 
 const routes = [
    // Default route: Redirect to '/connexion/login'
@@ -10,6 +11,7 @@ const routes = [
   // Connexions routes
   { path: '/connexion/login', name:"login" , component: Login },
   { path: '/connexion/forgotten', name:"forgotten" , component: Forgotten },
+  { path: '/home', name:"home" , component: Home },
 ]
 
 const router = createRouter({
