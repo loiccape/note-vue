88c29f37c2be876758a8f984678a6ec03c7df8b1
Loïc Cape <loiccape.pro@outlook.fr>
2024-11-19 13:21:52 +0100

    ajout id au model

--- src/models/NoteModel.ts
+++ src/models/NoteModel.ts
@@ -1,24 +1,20 @@
 export default class NoteModel {
-    title: string;
-    tags: string[];
-    lastEdited: Date;
-    content: string;
-  
-    constructor(title: string, tags: string[], lastEdited: Date, content: string) {
-      this.title = title;
-      this.tags = tags;
-      this.lastEdited = lastEdited;
-      this.content = content;
-    }
-  
-    // Méthode pour créer une instance de NoteModel à partir d'un objet JSON
-    static fromJSON(json: any): NoteModel {
-      return new NoteModel(
-        json.title,
-        json.tags,
-        new Date(json.lastEdited),
-        json.content
-      );
-    }
+  id: string
+  title: string
+  tags: string[]
+  lastEdited: Date
+  content: string
+
+  constructor(id:string,title: string, tags: string[], lastEdited: Date, content: string) {
+    this.id = id
+    this.title = title
+    this.tags = tags
+    this.lastEdited = lastEdited
+    this.content = content
   }
-  
+
+  // Méthode pour créer une instance de NoteModel à partir d'un objet JSON
+  static fromJSON(json: any): NoteModel {
+    return new NoteModel(json.id,json.title, json.tags, new Date(json.lastEdited), json.content)
+  }
+}
