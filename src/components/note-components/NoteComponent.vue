eccf7f1594720e7a338ee697bf27d0e536bb1ec8
Loïc Cape <loiccape.pro@outlook.fr>
2024-11-25 14:14:07 +0100

    nopte page

--- src/components/note-components/NoteComponent.vue
+++ src/components/note-components/NoteComponent.vue
@@ -1,22 +1,13 @@
 <template>
-    <div @click="goToNoteDetail(note.id)" class="p-1 text-text border-b-2">
+    <div class="p-4 text-text border-b-2 w-full">
       <p class=" text-1xl md:text-2xl font-semibold">{{ note.title }}</p>
-      <Badge v-for="tag in note.tags" :key="tag" class="mr-2 mt-2">{{ tag }}</Badge>
-      <!-- <p>{{ note.content }}</p> -->
+      <span v-for="tag in note.tags" :key="tag" class="mr-2 p-1 bg-background border">{{ tag }}</span>
       <p class="mt-1 text-slate-500">{{ note.getFormattedDate() }}</p>
     </div>
   </template>
   
   <script setup lang="ts">
   import NoteModel from '@/models/NoteModel';
-  import { useRouter } from 'vue-router';
-
-  //imports shadcn
-  import { Badge } from '@/components/ui/badge'
-
-
-  // Utilisation du composable useRouter
-  const router = useRouter();
   
 
 defineProps<{
@@ -24,9 +15,7 @@ defineProps<{
   }>();
   
 
-const goToNoteDetail = (noteId:string) => {
-  router.push({ name: 'noteDetail', params: { id: noteId } });
-};
+
   </script>
   
  
